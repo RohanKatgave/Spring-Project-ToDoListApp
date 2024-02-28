fbedba6636d03631036632b487e574e859fea5d9
Rohan <rohankatgave774@gmail.com>
2024-02-28 16:17:36 +0530

    Initial commit

---
+++ ToDo-App/src/main/java/com/example/ToDoApp/ToDoApp1.java
@@ -0,0 +1,13 @@
+package com.example.ToDoApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoApp1 {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToDoApp1.class, args);
+	}
+
+}
---
+++ ToDo-App/src/test/java/com/example/ToDoApp/ToDoApp1Tests.java
@@ -0,0 +1,13 @@
+package com.example.ToDoApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoApp1Tests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
